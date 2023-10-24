a32e51abdd21c823b0e93634fab2c189492b20a8
jjbush11 <112502062+jjbush11@users.noreply.github.com>
2023-10-24 13:34:46 -0400

    comments added to datetime_convert.py

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
+# Define date string
 date_str = "2022-03-17 10:45:30"
+# Format date time in Y - m - d hours:min:second
 date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
 formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
 
