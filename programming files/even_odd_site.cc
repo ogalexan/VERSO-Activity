e095c1589524ac39eb3a1c97ba8e4dcbdba8b6e1
jjbush11 <112502062+jjbush11@users.noreply.github.com>
2023-10-24 13:36:11 -0400

    Update even_odd_site.cc

--- programming files/even_odd_site.cc
+++ programming files/even_odd_site.cc
@@ -3,10 +3,13 @@
 using namespace std;
 
 int main() {
+    // Open file
     ofstream file;
     file.open("numbers.html");
+    // Enter html into file 
     file << "<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
     file << "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
+    // Build table in file
     for (int i = 1; i <= 50; i++) {
         if (i % 2 == 0) {
             file << "<tr><td>" << i << "</td><td></td></tr>\n";
@@ -15,6 +18,7 @@ int main() {
             file << "<tr><td></td><td>" << i << "</td></tr>\n";
         }
     }
+    // Close table tags and close the file 
     file << "</table>\n</body>\n</html>";
     file.close();
     ifstream input("numbers.html");
